85dc777669826005e691ed10dc459c0dd807c7a8
Dang Le <dang.le.tech2403@gmail.com>
2025-07-22 20:53:33 -0500

    Complete setup for deployment

--- server/server.js
+++ server/server.js
@@ -6,9 +6,12 @@ import messageRouter from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +20,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")))
+
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+})
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
